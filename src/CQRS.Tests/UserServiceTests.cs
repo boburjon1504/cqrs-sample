3329c7e519113b98b253429b49b28b6fee952d36
Bobur <Bobur@BOBUR>
2025-03-26 12:26:48 +0500

    tests were added

---
+++ src/CQRS.Tests/UserServiceTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CQRS.Tests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void IsUserEqualToOrNot()
+    {
+        var a = 1;
+
+        Assert.Equal(1, a);
+    }
+    [Fact]
+    public void IsMultWorkingCorrectly()
+    {
+        var a = 50;
+        var b = 80;
+
+        Assert.NotEqual(0, a * b);
+    }
+}
